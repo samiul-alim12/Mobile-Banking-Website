a9b53374f00157cd92882e3e85804a1d84005fe7
samiul-alim12 <nawabitsamiul@gmail.com>
2025-02-24 21:26:19 +0600

    added login page to main home page

--- script/login.js
+++ script/login.js
@@ -1,3 +1,16 @@
-document.getElementById("login-btn" ).addEventListener('click',function(){
-  
+document.getElementById("login-btn" ).addEventListener("click",function(event){
+    event.preventDefault()
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number" ).value;
+    if(accountNumber.length === 11){
+        if(pinNumber.length === 6){
+            window.location.href = "main.html"
+        }
+        else{
+            alert("incorrect pin number")
+        }
+    }
+    else{
+        alert("Incorrect Number")
+    }
 })
